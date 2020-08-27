9321deadb2962c3568c734ce20969305274ae499
kasiaOla <67895860+kasiaOla@users.noreply.github.com>
2020-08-27 19:17:30 +0200

    refactor / added comments and Promise call

--- sciagawka/Observable-vs-Subject-vs-Behaviorsubject-vs-Replaysubject-vs-AsyncSubject.ts
+++ sciagawka/Observable-vs-Subject-vs-Behaviorsubject-vs-Replaysubject-vs-AsyncSubject.ts
@@ -4,15 +4,39 @@ import { BehaviorSubject } from 'rxjs';
 import { ReplaySubject } from 'rxjs';
 import { AsyncSubject } from 'rxjs';
 
-console.log('--- Observable');
-const observable = new Observable(observer => {
+const promise = new Promise((resolve, reject) => {
+  console.log('Promise zawsze się wykona');
+  setTimeout(() => {
+    resolve('foo');
+  }, 300);
+});
+
+promise.then((value) => {
+  console.log(value);
+});
+console.log('Promise jest dostępny w jednym z tych stanów: ' +
+'\n pending: stan inicjacji, ani wypełnione, ani odrzucone. ' +
+'\n fulfilled: operacja zakończona sukcesem. ' +
+'\n rejected: operacja zakończona porażką. \n' );
+
+console.log('\n--- Observable');
+const observable0 = new Observable(observer => {
   observer.next('1 Observable');
   observer.next('2 Observable');
   observer.next('3 Observable');
+  setTimeout(() => {
+    observer.next('4 Observable');
+    observer.complete();
+  }, 1000);
 });
-observable.subscribe(value => console.log(value));
-console.log(' ');
-
+observable0.subscribe({
+  next(x) { console.log('pobierz wartości ' + x); },
+  error(err) { console.error('Error: ' + err); },
+  complete() { console.log('done'); }
+});
+console.log('Observables to (lazy Push ) leniwe kolekcje push wypychające jedną lub wiele wartości. Promise zawsze 1 (colback fukcji)' +
+'\n Aby wywołać Observable trzeba zasubskrybować. ' +
+'\n Observables mogą dostarczać wartości synchronicznie lub asynchronicznie' );
 
 console.log('--- Subject');
 const subject = new Subject();
@@ -20,7 +44,7 @@ subject.next('1 subject.next');
 subject.subscribe(value => console.log('subject.subscribe value =>  ', value));
 subject.next('2 subject.next');
 subject.next('3 subject.next');
-console.log(' Subject nie wyświetla emitowanych wartości przed subscribe \n');
+console.log(' Subject NIE WYŚWIETLA emitowanych wartości przed subscribe \n');
 subject.complete();
 
 
@@ -31,20 +55,20 @@ behaviorSubject.subscribe(value => console.log('behaviorSubject.subscribe value 
 behaviorSubject.next('3 BehaviorSubject');
 behaviorSubject.next('4 BehaviorSubject');
 behaviorSubject.complete();
-console.log('Jeśli nie bedziemy emitować przed subscribe, wyświetli zainicjalizowaną wartość. ' +
+console.log(' Jeśli nie bedziemy emitować przed subscribe, wyświetli zainicjalizowaną wartość. ' +
 '\n W przeciwnym przypadku wyświetli wyemitowaną wartość z przed subscribe. \n');
 
 
 console.log('--- ReplaySubject');
-const replaySubject = new ReplaySubject();
-replaySubject.next('1 ReplaySubject!');
-replaySubject.next('2 ReplaySubject!');
-replaySubject.next('3 ReplaySubject!');
-replaySubject.next('4 ReplaySubject!');
-replaySubject.next('5 ReplaySubject!');
+const replaySubject = new ReplaySubject(2);
+replaySubject.next('1 ReplaySubject');
+replaySubject.next('2 ReplaySubject');
+replaySubject.next('3 ReplaySubject');
+replaySubject.next('4 ReplaySubject');
+replaySubject.next('5 ReplaySubject');
 replaySubject.subscribe(value => console.log(value));
-replaySubject.next('6 ReplaySubject!');
-replaySubject.next('7 ReplaySubject!');
+replaySubject.next('6 ReplaySubject');
+replaySubject.next('7 ReplaySubject');
 behaviorSubject.complete();
 console.log(' Dodając parametr w ReplaySubject(2); zwróci 2 ostatnie wartości przed subscribe. ' +
   '\n Bez parametru zwraca wszystkie emitowane wartości. \n');
@@ -60,5 +84,5 @@ asyncSubject.next('3 AsyncSubject');
 asyncSubject.next('4 AsyncSubject');
 asyncSubject.next('5 AsyncSubject');
 asyncSubject.complete();
-console.log('AsyncSubject musi zawierać metodę complete(), aby pobrać ostatnią wartości z subscribe. \n ' +
-'Dodanie kolejnych subscribe powoduję wyświetlenie tyle razy ostatniej wartości ile subscrybcji ');
+console.log(' AsyncSubject musi zawierać metodę complete(), aby pobrać ostatnią wartości z subscribe. \n ' +
+'Dodanie kolejnych subscribe powoduję wyświetlenie tyle razy ostatniej wartości ile subscrybcji \n');
